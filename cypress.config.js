936e58ce4d270e6ccad67f26e74529e242d42e7b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-16 09:52:47 -0400

    initial code

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    // baseUrl, etc
+    fixturesFolder: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      // and load any plugins that require the Node environment
+    },
+  },
+})
